782a6dcaf13b846af7eaa7f121e23350bfa73e44
noah <nomaxx117@gmail.com>
2020-03-16 09:39:41 -0500

    Wrote hello world program to blink LEDs

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,110 @@
 #![no_main]
 #![no_std]
 
+// On dev builds, go to debugger on panic
+#[cfg(debug_assertions)]
 extern crate panic_semihosting;
 
-use cortex_m_semihosting::{debug, hprintln};
+// On release builds, halt on panic
+#[cfg(not(debug_assertions))]
+extern crate panic_halt;
 
+use cortex_m::peripheral::DWT;
+use f3::hal::gpio::GpioExt;
+use f3::hal::rcc::RccExt;
+use f3::hal::stm32f30x;
+use f3::led::Leds;
+use rtfm::cyccnt::{Instant, U32Ext};
 
-#[rtfm::app(device = stm32f3)]
+const LED_PERIOD: u32 = 2_000_000;
+const LED_ON_TIME: u32 = 1_000_000;
+
+#[rtfm::app(device = stm32f30x, monotonic = rtfm::cyccnt::CYCCNT)]
 const APP: () = {
-    #[init]
-    fn init(_: init::Context) {
-        hprintln!("init").unwrap();
+    struct Resources {
+        leds: Leds,
     }
 
-    #[idle]
-    fn idle(_: idle::Context) -> ! {
-        loop {
-            cortex_m::asm::nop();
+    /// Initialization section
+    ///
+    /// Initializes our tasks and peripherals.
+    #[init(schedule = [blink_led, turn_off_led])]
+    fn init(mut cx: init::Context) -> init::LateResources {
+        // Initialize (enable) the monotonic timer (CYCCNT)
+        cx.core.DCB.enable_trace();
+
+        // required on Cortex-M7 devices that software lock the DWT (e.g. STM32F7)
+        DWT::unlock();
+        cx.core.DWT.enable_cycle_counter();
+
+        // grab current time
+        let now = Instant::now();
+
+        // schedule turn on led to happen immediately after init
+        cx.schedule.blink_led(now).unwrap();
+
+        // schedule led turn off to occur after our duty period
+        cx.schedule.turn_off_led(now + LED_ON_TIME.cycles()).unwrap();
+
+        // get peripherals
+        let board_peripherals = stm32f30x::Peripherals::take().unwrap();
+
+        // acquire the RCC
+        let mut rcc = board_peripherals.RCC.constrain();
+
+        // acquire GPIOE, which operates the LEDs on this board
+        let led_gpio = board_peripherals.GPIOE.split(&mut rcc.ahb);
+
+        // create an LEDs object from the GPIO
+        let leds = f3::led::Leds::new(led_gpio);
+
+        // return resources
+        init::LateResources {
+            leds
         }
+    }
+
+    /// Turns the LED on.
+    ///
+    /// This task reschedules itself, and then turns on the LEDs.
+    #[task(schedule = [blink_led], resources = [leds])]
+    fn blink_led(cx: blink_led::Context) {
+        // reschedule this task
+        // this is done with the scheduled time rather than current clock time, preventing drift.
+        cx.schedule.blink_led(cx.scheduled + LED_PERIOD.cycles()).unwrap();
+
+        // grab leds
+        let leds: &mut Leds = cx.resources.leds;
+
+        // turn on leds
+        for i in 0..leds.len() {
+            leds[i].on();
+        }
+    }
+
+    /// Turns the LED off.
+    ///
+    /// This task reschedules itself, and then turns off the LEDs.
+    #[task(schedule = [turn_off_led], resources = [leds])]
+    fn turn_off_led(cx: turn_off_led::Context) {
+        // reschedule this task
+        // this is done with the scheduled time rather than current clock time, preventing drift.
+        cx.schedule.turn_off_led(cx.scheduled + LED_PERIOD.cycles()).unwrap();
+
+        // grab leds
+        let leds: &mut Leds = cx.resources.leds;
+
+        // turn off leds
+        for i in 0..leds.len() {
+            leds[i].off();
+        }
+    }
+
+    /// Declare free interrupts
+    /// These are used to dispatch software-spawned tasks
+    /// These are interrupts not used by hardware.
+    extern "C" {
+        fn CAN_RX1();
+        fn CAN_RX2();
     }
 };
